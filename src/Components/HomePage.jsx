75eac648abce2e0e81a41af5b9baa4a370981350
Emanuele DelBono <emanuele@codiceplastico.com>
2023-06-29 15:54:59 +0200

    ref

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,36 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import axios from 'axios'
 
 const HomePage = () => {
 
   const [user, setUser] = useState({username: '', password: ''})
-
-  const handleChangeUser = (evt) => {
-    setUser({...user, [evt.target.name]: evt.target.value})
-  }
+  const myUsername = useRef('')
+  const myPassword = useRef('')
 
 
   const submit = () => {
-
-    if (user.password.length < 8) {
-      alert('password troppo corta')
-      return
-    }
-
-    console.log('submit', user)
-    // get username e pwd
-    // post verso API
+    const username = myUsername.current.value
+    const password = myPassword.current.value
   }
 
   return (
     <div>
-      <input type="text" name="username" defaultValue={user.username} onChange={handleChangeUser} />
-      <input type="text" name="password" defaultValue={user.password} onChange={handleChangeUser} />
-
+      <input type="text" name="username" defaultValue={user.username} ref={myUsername}/>
+      <input type="text" name="password" defaultValue={user.password} ref={myPassword}/>
 
       <button onClick={submit}>Login</button>
-
-
     </div>
     )
 }
