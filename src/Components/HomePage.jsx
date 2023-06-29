98404fc92f9f606719881dbc93716d335b0090ff
Emanuele DelBono <emanuele@codiceplastico.com>
2023-06-29 15:31:51 +0200

    counter

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const HomePage = () => {
+  const [counter, setCounter] = useState(0)
+
+  const handlePlusClick = () => {
+    setCounter(counter + 1)
+  }
+
   return (
     <div>
-      <h2>Home page</h2>
+      <div>
+        <h3>{counter}</h3>
+      </div>
+      <div>
+        <button>-</button>
+        <button onClick={handlePlusClick}>+</button>
+      </div>
     </div>
     )
 }
